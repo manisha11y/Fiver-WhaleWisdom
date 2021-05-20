829f51aa0bac2a8709d876c81e2830fb176070c5
manisha11y <csmanisha.4ai@gmail.com>
2021-05-20 15:24:42 +0530

    added schema for rest endpoints

--- cloud_function/config.py
+++ cloud_function/config.py
@@ -1,36 +1,37 @@
-from filers_metadata import *
 import google.cloud.bigquery as bigquery
 
-class Config():
-    endpoint_args_list = [
-        # '{"command":"quarters"}',
-        # '{"command":"stock_lookup", "name":"Apple Comp"}',
-        # '{"command":"filer_lookup", "name":"berkshire"}',
-        # '{"command":"stock_comparison","stockid":3598,"q1id":39,"q2id":40,"order":"q2_shares","dir":"DESC"}',
-        # '{"command":"holdings_comparison","filerid":163,"q1id":39,"q2id":40}',
-        '{"command":"holdings","filer_ids":[289989,2911,297583,279570,328606,70329,48797,2715,1238,102758],"all_quarters":1}'
-        '{"command":"holdings","filer_ids":[152,163719,1249,22,96740,289090,358445,319105,328336,328353],"all_quarters":1}'
-        '{"command":"holdings","filer_ids":[336386,327443,166331,1226,1163,328436,261,358382,264203,895],"all_quarters":1}'
-        '{"command":"holdings","filer_ids":[75916,221641,66684,143033,287250,1833,200259,256630,289089,287818],"all_quarters":1}'
-        '{"command":"holdings","filer_ids":[366313,358409,288840,334872,135224,2514,178001,106401,208467,358129],"all_quarters":1}'
-        '{"command":"holdings","filer_ids":[2395,327965,297371,2358,200181,288864,66809,358608,288798,211631],"all_quarters":1}'
-        '{"command":"holdings","filer_ids":[93698,256486,122,327881,298386,289052,249588,215421,197090,339162],"all_quarters":1}'
-        # '{"command":"holders","stock_ids":[195,411],"include_13d":1}'
 
-    ]
+class Config():
 
     secret_key = 'LKuMSmJ0Y44rzP4PjKKAE3iOTnbfVaDvyO9yiseL'
     shared_key = 'AE9I6evS7Yhrc9OmxCNY'
     project_id = 'whalewisdom'
-    
+
+    endpoint_args_list = [
+        '{"command":"quarters"}',
+        '{"command":"stock_lookup", "name":"Apple Comp"}',
+        '{"command":"filer_lookup", "name":"berkshire"}',
+        '{"command":"stock_comparison","stockid":3598,"q1id":39,"q2id":40,"order":"q2_shares","dir":"DESC"}',
+        '{"command":"holdings_comparison","filerid":163,"q1id":39,"q2id":40}',
+        # '{"command":"holdings","filer_ids":[289989,2911,297583,279570,328606,70329,48797,2715,1238,102758],"all_quarters":1}'
+        # '{"command":"holdings","filer_ids":[152,163719,1249,22,96740,289090,358445,319105,328336,328353],"all_quarters":1}'
+        # '{"command":"holdings","filer_ids":[336386,327443,166331,1226,1163,328436,261,358382,264203,895],"all_quarters":1}'
+        # '{"command":"holdings","filer_ids":[75916,221641,66684,143033,287250,1833,200259,256630,289089,287818],"all_quarters":1}'
+        # '{"command":"holdings","filer_ids":[366313,358409,288840,334872,135224,2514,178001,106401,208467,358129],"all_quarters":1}'
+        # '{"command":"holdings","filer_ids":[2395,327965,297371,2358,200181,288864,66809,358608,288798,211631],"all_quarters":1}'
+        # '{"command":"holdings","filer_ids":[93698,256486,122,327881,298386,289052,249588,215421,197090,339162],"all_quarters":1}'
+        '{"command":"holders","stock_ids":[195,411]}'
+
+    ]
+
     # bigquery Config
     bg_dataset = "endpoints_data"
 
     # Holdings Schema
-    holdings_schema=[
+    holdings_schema = [
         bigquery.SchemaField("filer_name", "STRING", mode="NULLABLE"),
         bigquery.SchemaField("filer_id", "INTEGER", mode="NULLABLE"),
-        
+
         bigquery.SchemaField(
             "records",
             "RECORD",
@@ -38,42 +39,202 @@ class Config():
             fields=[
                 bigquery.SchemaField("quarter_id",	"INTEGER",	mode="NULLABLE"),
                 bigquery.SchemaField("quarter",	"DATE",	mode="NULLABLE"),
-                bigquery.SchemaField("date_last_filed",	"DATE",	mode="NULLABLE"),
                 bigquery.SchemaField(
-                "holdings",
-                "RECORD",
-                mode="REPEATED",
+                    "date_last_filed",	"DATE",	mode="NULLABLE"),
+                bigquery.SchemaField(
+                    "holdings",
+                    "RECORD",
+                    mode="REPEATED",
                 fields=[
-                    bigquery.SchemaField("quarter_id_owned",	"INTEGER",	mode="NULLABLE"),
-                    bigquery.SchemaField("percent_change",	"FLOAT",	mode="NULLABLE"),
-                    bigquery.SchemaField("avg_price",	"FLOAT",	mode="NULLABLE"),
-                    bigquery.SchemaField("filer_state",	"STRING",	mode="NULLABLE"),
-                    bigquery.SchemaField("source_date",	"DATE",	mode="NULLABLE"),
-                    bigquery.SchemaField("stock_id",	"INTEGER",	mode="NULLABLE"),
-                    bigquery.SchemaField("previous_shares",	"FLOAT",	mode="NULLABLE"),
-                    bigquery.SchemaField("current_shares",	"FLOAT",	mode="NULLABLE"),
-                    bigquery.SchemaField("shares_change",	"FLOAT",	mode="NULLABLE"),
-                    bigquery.SchemaField("source",	"STRING",	mode="NULLABLE"),
-                    bigquery.SchemaField("previous_mv",	"FLOAT",	mode="NULLABLE"),
-                    bigquery.SchemaField("current_percent_of_portfolio",	"FLOAT",	mode="NULLABLE"),
-                    bigquery.SchemaField("previous_percent_of_portfolio",	"FLOAT",	mode="NULLABLE"),
-                    bigquery.SchemaField("current_mv",	"FLOAT",	mode="NULLABLE"),
-                    bigquery.SchemaField("previous_ranking",	"INTEGER",	mode="NULLABLE"),
-                    bigquery.SchemaField("filer_city",	"STRING",	mode="NULLABLE"),
-                    bigquery.SchemaField("filer_street_address",	"STRING",	mode="NULLABLE"),
-                    bigquery.SchemaField("percent_ownership",	"FLOAT",	mode="NULLABLE"),
-                    bigquery.SchemaField("filer_zip_code",	"STRING",	mode="NULLABLE"),
-                    bigquery.SchemaField("sector",	"STRING",	mode="NULLABLE"),
-                    bigquery.SchemaField("current_ranking",	"INTEGER",	mode="NULLABLE"),
-                    bigquery.SchemaField("industry",	"STRING",	mode="NULLABLE"),
-                    bigquery.SchemaField("position_change_type",	"STRING",	mode="NULLABLE"),
-                    bigquery.SchemaField("security_type",	"STRING",	mode="NULLABLE"),
-                    bigquery.SchemaField("stock_ticker",	"STRING",	mode="NULLABLE"),
-                    bigquery.SchemaField("filer_id",	"INTEGER",	mode="NULLABLE"),
-                    bigquery.SchemaField("stock_name",	"STRING",	mode="NULLABLE"),
-                    bigquery.SchemaField("filer_name",	"STRING",	mode="NULLABLE"),
-                ],
-            ),
-        ],
+                     bigquery.SchemaField(
+                            "quarter_id_owned","INTEGER", mode="NULLABLE"),
+                     bigquery.SchemaField(
+                            "percent_change","FLOAT", mode="NULLABLE"),
+                     bigquery.SchemaField(
+                            "avg_price","FLOAT", mode="NULLABLE"),
+                     bigquery.SchemaField(
+                            "filer_state","STRING", mode="NULLABLE"),
+                     bigquery.SchemaField(
+                            "source_date","DATE", mode="NULLABLE"),
+                     bigquery.SchemaField(
+                            "stock_id","INTEGER", mode="NULLABLE"),
+                     bigquery.SchemaField(
+                            "previous_shares","FLOAT", mode="NULLABLE"),
+                     bigquery.SchemaField(
+                            "current_shares","FLOAT", mode="NULLABLE"),
+                     bigquery.SchemaField(
+                            "shares_change","FLOAT", mode="NULLABLE"),
+                     bigquery.SchemaField(
+                            "source","STRING", mode="NULLABLE"),
+                     bigquery.SchemaField(
+                            "previous_mv","FLOAT", mode="NULLABLE"),
+                     bigquery.SchemaField(
+                            "current_percent_of_portfolio","FLOAT", mode="NULLABLE"),
+                     bigquery.SchemaField(
+                            "previous_percent_of_portfolio","FLOAT", mode="NULLABLE"),
+                     bigquery.SchemaField(
+                            "current_mv","FLOAT", mode="NULLABLE"),
+                     bigquery.SchemaField(
+                            "previous_ranking","INTEGER", mode="NULLABLE"),
+                     bigquery.SchemaField(
+                            "filer_city","STRING", mode="NULLABLE"),
+                     bigquery.SchemaField(
+                            "filer_street_address","STRING", mode="NULLABLE"),
+                     bigquery.SchemaField(
+                            "percent_ownership","FLOAT", mode="NULLABLE"),
+                     bigquery.SchemaField(
+                            "filer_zip_code","STRING", mode="NULLABLE"),
+                     bigquery.SchemaField(
+                            "sector","STRING", mode="NULLABLE"),
+                     bigquery.SchemaField(
+                            "current_ranking","INTEGER", mode="NULLABLE"),
+                     bigquery.SchemaField(
+                            "industry","STRING", mode="NULLABLE"),
+                     bigquery.SchemaField(
+                            "position_change_type","STRING", mode="NULLABLE"),
+                     bigquery.SchemaField(
+                            "security_type","STRING", mode="NULLABLE"),
+                     bigquery.SchemaField(
+                            "stock_ticker","STRING", mode="NULLABLE"),
+                     bigquery.SchemaField(
+                            "filer_id","INTEGER", mode="NULLABLE"),
+                     bigquery.SchemaField(
+                            "stock_name","STRING", mode="NULLABLE"),
+                     bigquery.SchemaField(
+                            "filer_name","STRING", mode="NULLABLE"),
+                    ],
+                ),
+            ],
         ),
-        ]
+    ]
+
+    quarters_schema = [
+        bigquery.SchemaField("status",	"STRING",	mode="NULLABLE"),
+        bigquery.SchemaField("filing_period",	"DATE",	mode="NULLABLE"),
+        bigquery.SchemaField("id",	"INTEGER",	mode="NULLABLE")
+
+    ]
+
+    stock_lookup_schema = [
+        bigquery.SchemaField("status",	"STRING",	mode="NULLABLE"),
+        bigquery.SchemaField("name",	"STRING",	mode="NULLABLE"),
+        bigquery.SchemaField("link",	"STRING",	mode="NULLABLE"),
+        bigquery.SchemaField("cusip",	"STRING",	mode="NULLABLE"),
+        bigquery.SchemaField("ticker",	"STRING",	mode="NULLABLE"),
+        bigquery.SchemaField("id",	"INTEGER",	mode="NULLABLE")
+    ]
+
+    filer_lookup_schema = [
+        bigquery.SchemaField("state_incorporation",	"STRING", mode="NULLABLE"),
+        bigquery.SchemaField("city", "STRING", mode="NULLABLE"),
+        bigquery.SchemaField("link", "STRING", mode="NULLABLE"),
+        bigquery.SchemaField("street_address", "STRING", mode="NULLABLE"),
+        bigquery.SchemaField("business_phone", "STRING", mode="NULLABLE"),
+        bigquery.SchemaField("state", "STRING", mode="NULLABLE"),
+        bigquery.SchemaField("id", "INTEGER", mode="NULLABLE"),
+        bigquery.SchemaField("cik", "INTEGER", mode="NULLABLE"),
+        bigquery.SchemaField("zip_code", "STRING", mode="NULLABLE"),
+        bigquery.SchemaField("name", "STRING", mode="NULLABLE"),
+        bigquery.SchemaField("irs_number", "INTEGER", mode="NULLABLE"),
+    ]
+
+    stock_comparison_schema = [
+        bigquery.SchemaField("security_type", "STRING", mode="NULLABLE"),
+        bigquery.SchemaField("change_in_shares", "FLOAT", mode="NULLABLE"),
+        bigquery.SchemaField("quarter_two_shares", "FLOAT", mode="NULLABLE"),
+        bigquery.SchemaField("filer_id", "INTEGER", mode="NULLABLE"),
+        bigquery.SchemaField("quarter_one_shares", "FLOAT", mode="NULLABLE"),
+        bigquery.SchemaField("filer_name", "STRING", mode="NULLABLE"),
+    ]
+
+    holdings_comparison_schema = [
+        bigquery.SchemaField("quarter_two_market_value","INTEGER", mode="NULLABLE"),
+        bigquery.SchemaField("quarter_two_shares", "FLOAT", mode="NULLABLE"),
+        bigquery.SchemaField("change_in_shares", "FLOAT", mode="NULLABLE"),
+        bigquery.SchemaField("quarter_one_percent_of_portfolio", "FLOAT", mode="NULLABLE"),
+        bigquery.SchemaField("security_type", "STRING", mode="NULLABLE"),
+        bigquery.SchemaField("stock_id", "INTEGER", mode="NULLABLE"),
+        bigquery.SchemaField("stock_name", "STRING", mode="NULLABLE"),
+        bigquery.SchemaField("quarter_one_shares", "FLOAT", mode="NULLABLE"),
+        bigquery.SchemaField(
+            "quarter_two_percent_of_portfolio", "FLOAT", mode="NULLABLE"),
+        bigquery.SchemaField("quarter_one_market_value", "INTEGER", mode="NULLABLE"),
+        bigquery.SchemaField("symbol", "STRING", mode="NULLABLE")
+    ]
+
+    holders_schema = [
+        bigquery.SchemaField("stock_id", "INTEGER",	mode="NULLABLE"),
+        bigquery.SchemaField("stock_name", "STRING", mode="NULLABLE"),
+        bigquery.SchemaField(
+            "records",
+            "RECORD",
+            mode="REPEATED",
+            fields=[bigquery.SchemaField("quarter_id", "INTEGER", mode="NULLABLE"),
+                    bigquery.SchemaField("quarter", "DATE", mode="NULLABLE"),
+                    bigquery.SchemaField(
+                "holdings",
+                        "RECORD",
+                        mode="REPEATED",
+                        fields=[
+                            bigquery.SchemaField(
+                                "filer_state", "STRING", mode="NULLABLE"),
+                            bigquery.SchemaField(
+                                "filer_city", "STRING", mode="NULLABLE"),
+                            bigquery.SchemaField(
+                                "filer_street_address", "STRING", mode="NULLABLE"),
+                            bigquery.SchemaField(
+                                "percent_ownership", "FLOAT", mode="NULLABLE"),
+                            bigquery.SchemaField(
+                                "source_date", "DATE", mode="NULLABLE"),
+                            bigquery.SchemaField(
+                                "stock_ticker", "STRING", mode="NULLABLE"),
+                            bigquery.SchemaField(
+                                "filer_name", "STRING", mode="NULLABLE"),
+                            bigquery.SchemaField(
+                                "quarter_id_owned", "INTEGER", mode="NULLABLE"),
+                            bigquery.SchemaField(
+                                "avg_price", "FLOAT", mode="NULLABLE"),
+                            bigquery.SchemaField(
+                                "stock_id", "INTEGER", mode="NULLABLE"),
+                            bigquery.SchemaField(
+                                "previous_shares", "FLOAT", mode="NULLABLE"),
+                            bigquery.SchemaField(
+                                "current_shares", "FLOAT", mode="NULLABLE"),
+                            bigquery.SchemaField(
+                                "shares_change", "FLOAT", mode="NULLABLE"),
+                            bigquery.SchemaField(
+                                "source", "STRING", mode="NULLABLE"),
+                            bigquery.SchemaField(
+                                "previous_mv", "FLOAT", mode="NULLABLE"),
+                            bigquery.SchemaField(
+                                "current_percent_of_portfolio", "FLOAT", mode="NULLABLE"),
+                            bigquery.SchemaField(
+                                "previous_percent_of_portfolio", "FLOAT", mode="NULLABLE"),
+                            bigquery.SchemaField(
+                                "current_mv", "FLOAT", mode="NULLABLE"),
+                            bigquery.SchemaField(
+                                "previous_ranking", "INTEGER", mode="NULLABLE"),
+                            bigquery.SchemaField(
+                                "filer_zip_code", "STRING", mode="NULLABLE"),
+                            bigquery.SchemaField(
+                                "sector", "STRING", mode="NULLABLE"),
+                            bigquery.SchemaField(
+                                "current_ranking", "INTEGER", mode="NULLABLE"),
+                            bigquery.SchemaField(
+                                "industry", "STRING", mode="NULLABLE"),
+                            bigquery.SchemaField(
+                                "position_change_type", "STRING", mode="NULLABLE"),
+                            bigquery.SchemaField(
+                                "stock_name", "STRING", mode="NULLABLE"),
+                            bigquery.SchemaField(
+                                "filer_id", "INTEGER", mode="NULLABLE"),
+                            bigquery.SchemaField(
+                                "percent_change", "FLOAT", mode="NULLABLE"),
+                            bigquery.SchemaField(
+                                "security_type", "STRING", mode="NULLABLE")
+                        ],
+            ),
+            ],
+        ),
+
+    ]
--- cloud_function/main.py
+++ cloud_function/main.py
@@ -1,6 +1,7 @@
 import requests
 from urllib.parse import quote_plus
 from google.cloud import bigquery
+import logging
 import time
 import hashlib
 import hmac
@@ -29,37 +30,32 @@ class GCPIntegration():
         try:
             r = requests.get(url=api_url)
         except Exception as e:
-            print('Error while calling {} endpoint : {}'.format(endpoint_name, e))
+            logging.error('Error while calling {} endpoint : {}'.format(endpoint_name, e))
         
         if "errors" in r.text:
              return r.text, "FAILURE"
         elif r.status_code == 200:
              return r.json(), "SUCCESS"
         
-    def load_to_bigquery(self, table_name, file_name, table_schema=Config.holdings_schema):
+    def load_to_bigquery(self, table_name, file_name, table_schema):
 
         client = bigquery.Client()
         dataset_ref = client.dataset(Config.bg_dataset)
-        table_ref = dataset_ref.table(table_name)
-        print(file_name)
-        print(table_name)
+        table_ref = dataset_ref.table(table_name)        
+        
         try:
             job_config = bigquery.LoadJobConfig(
                 schema= table_schema,
                 source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON, 
             )
-            print("loading data...")
             with open(file_name, "rb") as source_file:
                     job = client.load_table_from_file(
                         source_file, table_ref, location="US", job_config=job_config)
-            print("finished loading")
             job.result()
         except Exception as e:
-            for e in job.errors:
-                print('ERROR: {}'.format(e['message']))
-                # print("Error loading the records in {} table : {}".format(table_ref, e))
+            logging.error("Error loading the records in {} table : {}".format(table_ref, e))
         else:
-             print("{} row(s) affected in {}".format(job.output_rows, table_name))
+            logging.info("{} row(s) affected in {}".format(job.output_rows, table_name))
 
        
 def main():
@@ -68,35 +64,49 @@ def main():
     for json_args in Config.endpoint_args_list:
 
         bq_table = json.loads(json_args)["command"]
-        # bq_schema = Config+ "." +bq_table+"_schema"
         response_file = bq_table + ".json"
-        response_json, status = gcp_integration.call_to_api(
+        endpoint_response, status = gcp_integration.call_to_api(
                 json_args, bq_table)
-        # print(next(iter(response_json.keys())))
         try:    
             if status == "SUCCESS":      
-                print("Request Successful to {}".format(bq_table))
+                logging.info("Request Successful to {}".format(bq_table))
                 # use '/tmp/'+response_file while deplying to cloud function :2 places
                 with open(response_file, "w") as outfile:
-                    for item in next(iter(response_json.values())):
+                    for item in next(iter(endpoint_response.values())):
                         json.dump(item, outfile)
                         outfile.write('\n')
-                
-                    # gcp_integration.load_to_bigquery(bq_table, response_file)
+                # Choose appropriate schema for endpoints data to load data in the right way
+                if bq_table == "holdings":
+                    bq_schema = Config.holdings_schema
+                elif bq_table == "quarters":
+                    bq_schema = Config.quarters_schema
+                elif  bq_table == "stock_lookup":
+                    bq_table = Config.stock_lookup_schema
+                elif  bq_table == "filer_lookup":
+                    bq_table = Config.filer_lookup_schema
+                elif  bq_table == "stock_comparison":
+                    bq_table = Config.stock_comparison_schema
+                elif  bq_table == "holdings_comparison":
+                    bq_table = Config.holdings_comparison_schema
+                elif  bq_table == "holdings":
+                    bq_table = Config.holdings_schema
+
+                gcp_integration.load_to_bigquery(bq_table, response_file, bq_schema)
             elif status == "FAILURE":
-                print('Error while calling {} endpoint : {}'
-                        .format(bq_table, response_json))
+                logging.error('Error while calling {} endpoint : {}'
+                        .format(bq_table, endpoint_response))
         except Exception as e:
-                print("Error loading the records in {} table : {}".format(bq_table, e))
+                logging.error("Error loading the records in {} table : {}".format(bq_table, e))
         # else:
-        #     print('{} Error while calling {} endpoint'.format(
+        #     logging.error('{} Error while calling {} endpoint'.format(
         #         status_code, bq_table))
 
 
+logging.basicConfig(level=logging.DEBUG)
 
 gcp_integration=GCPIntegration()
 # gcp_integration.main()
 # gcp_integration.load_to_bigquery("holdings", "holdings.json")
 main()
-gcp_integration.load_to_bigquery("holdings", "holdings.json")
+# gcp_integration.load_to_bigquery("holdings", "holdings.json")
 
